530666ae91882a381189073201b48dad4315b0c4
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2021-04-17 06:15:48 +0000

    Deploy lalifeier/admin-pro to github.com/lalifeier/admin-pro.git:gh-pages

---
+++ static/js/chunk-2d0cfaef.2628020d.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([["chunk-2d0cfaef"],{6582:function(n,t,e){"use strict";e.r(t);var a=e("2877"),l=Object(a.a)({},(function(n,t){var e=t._c;return e("div",{staticStyle:{padding:"30px"}},[e("el-alert",{attrs:{closable:!1,title:"menu 1-2-1",type:"warning"}})],1)}),[],!0,null,null,null);t.default=l.exports}}]);
