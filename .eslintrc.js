209c22e723f2773b00478bc01e5d689c1b7f4e9f
lalifeier <lalifeier@gmail.com>
2021-04-30 00:08:28 +0800

    feat: init

--- .eslintrc.js
+++ .eslintrc.js
@@ -6,11 +6,32 @@ module.exports = {
     es6: true,
   },
   extends: ['plugin:vue/recommended', 'plugin:prettier/recommended'],
+  // extends: ['plugin:vue/recommended', 'airbnb-base', 'plugin:prettier/recommended'],
   parserOptions: {
     parser: 'babel-eslint',
   },
   rules: {
+    'no-unused-expressions': ['error', { allowShortCircuit: true, allowTernary: true }],
+    'func-names': ['error', 'as-needed'],
+    'no-param-reassign': ['error', { props: false }],
+    'no-plusplus': 'off',
+    'no-shadow': 'off',
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'import/extensions': [
+      'error',
+      'always',
+      {
+        js: 'never',
+        vue: 'never',
+      },
+    ],
   },
+  // settings: {
+  //   'import/resolver': {
+  //     webpack: {
+  //       config: 'node_modules/@vue/cli-service/webpack.config.js',
+  //     },
+  //   },
+  // },
 }
